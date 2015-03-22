b3b1ac6e80c5debccf1bb4f28ece5a24f4d359e3
Ruchi Agarwal <ruchi.agarwalcse@gmail.com>
2015-03-22 12:30:29 -0700

    Final commit

---
+++ src/main/java/edu/sjsu/cmpe273/lab2/PollClient.java
@@ -0,0 +1,79 @@
+package edu.sjsu.cmpe273.lab2;
+
+import com.google.common.util.concurrent.SettableFuture;
+
+import io.grpc.ChannelImpl;
+import edu.sjsu.cmpe273.lab2.PollServiceGrpc.PollServiceBlockingStub;
+import edu.sjsu.cmpe273.lab2.PollServiceGrpc.PollServiceStub;
+import io.grpc.stub.StreamObserver;
+import io.grpc.transport.netty.NegotiationType;
+import io.grpc.transport.netty.NettyChannelBuilder;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * Sample client code that makes gRPC calls to the server.
+ */
+public class PollClient {
+    private static final Logger logger = Logger.getLogger(PollClient.class.getName());
+
+    private final ChannelImpl channel;
+    private final PollServiceBlockingStub blockingStub;
+    private final PollServiceStub asyncStub;
+
+    public PollClient(String host, int port) {
+        channel = NettyChannelBuilder.forAddress(host, port)
+                .negotiationType(NegotiationType.PLAINTEXT)
+                .build();
+        blockingStub = PollServiceGrpc.newBlockingStub(channel);
+        asyncStub = PollServiceGrpc.newStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTerminated(5, TimeUnit.SECONDS);
+    }
+
+
+    public void addPollService(String moderatorId, String question, String startedAt, String expiredAt, String[] choice) {
+        if (choice == null || choice.length < 2){
+            new RuntimeException("Choice must have two items in it.");
+        }
+        try {
+            logger.info("Creating a new poll for moderator " + moderatorId);
+            PollRequest request = PollRequest.newBuilder()
+                    .setModeratorId(moderatorId)
+                    .setQuestion(question)
+                    .setStartedAt(startedAt)
+                    .setExpiredAt(expiredAt)
+                    .addChoice(choice[0])
+                    .addChoice(choice[1]).build();
+            PollResponse response = blockingStub.createPoll(request);
+            logger.info("Created a new poll with id = " + response.getId());
+        } catch (RuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed", e);
+            return;
+        }
+    }
+
+
+   public static void main(String[] args) throws Exception {
+       PollClient client = new PollClient("localhost", 50051);
+        try {
+            String moderatorId = "1";
+            String question = "What type of smartphone do you have?";
+            String startedAt = "2015-02-23T13:00:00.000Z";
+            String expiredAt = "2015-02-24T13:00:00.000Z";
+            String[] choice = new String[] {"Android", "iPhone"};
+            client.addPollService(moderatorId, question, startedAt, expiredAt, choice);
+        } finally {
+            client.shutdown();
+        }
+    }
+
+}
